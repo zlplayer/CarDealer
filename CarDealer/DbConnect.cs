2825be0f703cc9c2976262c800650b66d732c4ce
zlplayer <konrad.boron@op.pl>
2023-06-13 21:33:33 +0200

    improved login form

--- CarDealer/DbConnect.cs
+++ CarDealer/DbConnect.cs
@@ -16,8 +16,8 @@ namespace CarDealer
         
         public string connection()
         {
-            //con = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Konzo\source\repos\CarDealer\CarDealer\dbCarDealer.mdf;Integrated Security=True;";
-            con = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Admin\source\repos\CarDealer\CarDealer\dbCarDealer.mdf;Integrated Security=True";
+            con = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Konzo\source\repos\CarDealer\CarDealer\dbCarDealer.mdf;Integrated Security=True;";
+            //con = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Admin\source\repos\CarDealer\CarDealer\dbCarDealer.mdf;Integrated Security=True";
             return con;
         }
         public void executeQuery(string sql)
--- CarDealer/LoginForm.Designer.cs
+++ CarDealer/LoginForm.Designer.cs
@@ -40,6 +40,10 @@
             this.btnClose = new Guna.UI2.WinForms.Guna2Button();
             this.txtPassword = new Guna.UI2.WinForms.Guna2TextBox();
             this.txtName = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -47,6 +51,10 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.label3);
@@ -197,6 +205,50 @@
             this.txtName.Size = new System.Drawing.Size(462, 36);
             this.txtName.TabIndex = 0;
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(216, 157);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(104, 29);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Witam w";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(29, 186);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(288, 29);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "dealerze samochodowym";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(224, 409);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(93, 16);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Developed by";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(228, 425);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(89, 16);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Konrad Boroń";
+            // 
             // LoginForm
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
@@ -216,6 +268,7 @@
             this.Text = "LoginForm";
             this.Load += new System.EventHandler(this.LoginForm_Load);
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -236,5 +289,9 @@
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.Button btnForGet;
         private Guna.UI2.WinForms.Guna2Button btnClose;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
     }
 }
